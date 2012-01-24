a4f87ecd015c3ac1bc526207f499361795e9f60e
Phil Crosby <phil.crosby@gmail.com>
2012-01-24 13:15:47 -0800

    An initial definition of the states to be used.

--- lib/build.rb
+++ lib/build.rb
@@ -1,2 +1,101 @@
+require "state_machine"
 class Build < Sequel::Model
+  @@regions = ["sandbox1", "sandbox2", "prod_region1", "prod_region2"]
+
+  def initialize()
+    super
+    self.current_region ||= @@regions.first
+  end
+
+  state_machine :state, :initial => :awaiting_deploy do
+
+    #
+    # Events and the state changes they cause.
+    #
+    event :begin_deploy do
+      transition :awaiting_deploy => :deploying
+    end
+
+    event :deploy_failed do
+      transition :deploying => :deploy_failed
+    end
+
+    event :deploy_succeeded do
+      transition :deploying => :ready_to_test
+    end
+
+    event :begin_testing do
+      transition :ready_to_test => :testing
+    end
+
+    event :testing_failed do
+      transition :testing => :testing_failed
+    end
+
+    event :testing_succeeded do
+      transition :testing => :monitoring, :if => :requires_monitoring?
+      transition :testing => :awaiting_deploy
+    end
+
+    event :monitoring_failed do
+      transition :monitoring => :monitoring_failed
+    end
+
+    event :monitoring_succeeded do
+      transition :monitoring => :awaiting_confirmation, :if => :requires_manual_deploy?
+      transition :monitoring => :deploying
+    end
+
+    event :manual_deploy_confirmed do
+      transition :awaiting_confirmation => :deploying
+    end
+
+    #
+    # Operations which occur when state changes.
+    #
+    after_transition any => :awaiting_deploy do |transition|
+      self.current_region = next_region
+    end
+
+    after_transition :on => :deploying do
+      schedule_deploy
+    end
+
+    after_transition :on => :monitoring do
+      enable_production_traffic_mirroring
+    end
+
+    after_transition :on => :deploy_failed do
+      notify_deploy_failed
+    end
+  end
+
+  def next_region
+    raise "This build has a region which is no longer defined" unless @@regions.include?(current_region)
+    next_region = @@regions[@@regions.index(self.current_region) + 1]
+    raise "Cannot pick a next region; this build's region is already the last." if next_region.nil?
+    next_region
+  end
+
+  def requires_manual_deploy?() self.current_region == "sandbox2" end
+
+  # The first sandbox is deployed to continuously and doesn't run production monitoring using mirroed traffic.
+  def requires_monitoring?() self.current_region != "sandbox1" end
+
+  def enable_production_traffic_mirroring
+    puts "enabling production traffic mirroring."
+  end
+
+  def schedule_deploy
+    puts "scheduling deploy to #{current_region}."
+  end
+
+  def staging_deploy_in_progress?
+    false
+  end
+
+  def notify_deploy_failed
+    puts "deploy to #{current_region} failed."
+  end
+
 end
