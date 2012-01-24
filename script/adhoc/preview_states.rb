9c8b3cab22a3e258d62baba9fb2c2458ce598cf2
Phil Crosby <phil.crosby@gmail.com>
2012-01-24 15:23:23 -0800

    Add a temporary developer script for previewing the state transition logic.

---
+++ script/adhoc/preview_states.rb
@@ -0,0 +1,15 @@
+# A temporary script for playing with the various states of a Build. Delete this once we begin writing tests.
+require "pathological"
+require "script/script_environment"
+
+puts "\n\n"
+
+build = Build.new
+events = [:begin_deploy, :deploy_succeeded, :begin_testing, :testing_succeeded, :monitoring_succeeded]
+events = events * 2
+
+events.each do |event|
+  puts "\ndispatching #{event}"
+  build.fire_events(event)
+  puts "state: #{build.state} region: #{build.current_region}"
+end
