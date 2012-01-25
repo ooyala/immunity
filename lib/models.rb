f0156bbe16abb72576461171eaf4823d64c9b8bd
Phil Crosby <phil.crosby@gmail.com>
2012-01-25 13:59:10 -0800

    Ignore log files from git

--- lib/models.rb
+++ lib/models.rb
@@ -5,7 +5,7 @@ PROTOCOL, DB_TYPE, DB_NAME, DB_HOST = DB_LOCATION.split(":")
 DB = Sequel.mysql(:host => DB_HOST, :user => DB_USER, :password => DB_PASSWORD, :database => DB_NAME)
 
 # This plugin gives you the "add_association_dependency" method, which lets you specify other objects to be
-# destroyed when the current model gets destroyed, e.g. when you delete a provider, also delete its movies.
+# destroyed when the current model gets destroyed, e.g. "when you delete a provider, also delete its movies".
 Sequel::Model.plugin :association_dependencies
 
 # Auto-populate "created_at" and "updated_at" fields.
