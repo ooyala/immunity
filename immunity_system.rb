42e9893db41c9fb9342c3bc51d7c1db78706b2aa
rui <rui@ooyala.com>
2012-01-27 11:42:38 -0800

    store the build status message in db.

--- immunity_system.rb
+++ immunity_system.rb
@@ -14,12 +14,24 @@ class ImmunitySystem < Sinatra::Base
   post "/deploy_succeed" do
     build = Build.first(:id => params[:build_id])
     build.fire_events(:deploy_succeeded)
-    buiild_status = DB[:build_status]
+    save_build_status(build.id, params[:message], '', '')
   end
 
   post "/deploy_failed" do
     build = Build.first(:id => params[:build_id])
     build.fire_events(:deploy_failed)
+    save_build_status(build.id, params[:message], '', '')
+  end
+
+  def save_build_status(build_id, stdout_text, stderr_text, message)
+    build_status = BuildStatus.first(:build_id => build.id)
+    if build_status.nil
+      build_status = BuildStatus.new(:build_id => build.id)
+    end
+    build_status.stdout = stdout_text
+    build_status.stderr = stderr_text
+    build_status.message = message
+    build_status.save
   end
 
 end
