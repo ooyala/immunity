729bfcd5a8fa816dec36f06fee08d5ab3de69022
rui <rui@ooyala.com>
2012-01-30 11:18:42 -0800

    add region to the build status table

---
+++ migrations/20120130111434_add_region_to_build_status.rb
@@ -0,0 +1,12 @@
+require "./" + File.join(File.dirname(__FILE__), "migration_helper")
+
+Sequel.migration do
+  up do
+    add_column :build_status, :region, String
+    add_index :build_status, :region, :unique => true
+  end
+
+  down do
+    drop_column :build_status, :region
+  end
+end
