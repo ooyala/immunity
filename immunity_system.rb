380466f80ef50e146b02229a1562bb4352c8fbb1
Phil Crosby <philc@ooyala.com>
2012-02-07 12:18:13 -0800

    Use nils instead of empty string as the response bodies for routes

--- immunity_system.rb
+++ immunity_system.rb
@@ -34,15 +34,14 @@ class ImmunitySystem < Sinatra::Base
     build.fire_events(:deploy_succeeded)
     save_build_status(build.id, params[:stdout], params[:stderr], params[:message], params[:region])
     build.fire_events(:begin_testing)
-    # TODO (Rui) Figure out why we need to return a string here, rest_client is throw exception for nil
-    ''
+    nil
   end
 
   post "/deploy_failed" do
     build = Build.first(:id => params[:build_id])
     save_build_status(build.id, params[:stdout], params[:stderr], params[:message], params[:region])
     build.fire_events(:deploy_failed)
-    ''
+    nil
   end
 
   post "/test_succeed" do
@@ -50,28 +49,28 @@ class ImmunitySystem < Sinatra::Base
     save_build_status(build.id, params[:stdout], params[:stderr], params[:message], params[:region])
     build.fire_events(:testing_succeeded)
     build.fire_events(:begin_deploy) if build.can_begin_deploy?
-    ''
+    nil
   end
 
   post "/test_failed" do
     build = Build.first(:id => params[:build_id])
     save_build_status(build.id, params[:stdout], params[:stderr], params[:message], params[:region])
     build.fire_events(:testing_failed)
-    ''
+    nil
   end
 
   post "/monitor_succeed" do
     build = Build.first(:id => params[:build_id])
     save_build_status(build.id, params[:stdout], params[:stderr], params[:message], params[:region])
     build.fire_events(:monitoring_succeeded)
-    ''
+    nil
   end
 
   post "/monitor_failed" do
     build = Build.first(:id => params[:build_id])
     save_build_status(build.id, params[:stdout], params[:stderr], params[:message], params[:region])
     build.fire_events(:monitoring_failed)
-    ''
+    nil
   end
 
   # Manually confirms that a build is OK and begins deploying it to prod3.
@@ -80,6 +79,7 @@ class ImmunitySystem < Sinatra::Base
     build = Build.first(:id => params[:build_id])
     build.fire_events(:manual_deploy_confirmed)
     build.fire_events(:begin_deploy)
+    nil
   end
 
   # Display helpers.
