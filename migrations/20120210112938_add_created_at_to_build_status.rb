50459b989759e4a0982b18f3eda109747b170c8b
Phil Crosby <philc@ooyala.com>
2012-02-10 11:32:47 -0800

    Add created_at to build_statuses

---
+++ migrations/20120210112938_add_created_at_to_build_status.rb
@@ -0,0 +1,11 @@
+require "./" + File.join(File.dirname(__FILE__), "migration_helper")
+
+Sequel.migration do
+  up do
+    add_column :build_statuses, :created_at, DateTime
+  end
+
+  down do
+    drop_column :build_statuses, :created_at
+  end
+end
