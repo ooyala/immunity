a749b942d2b730d5a50abc92e9bf089ad259402e
Phil Crosby <philc@ooyala.com>
2012-03-09 11:35:47 -0800

    Make checking for the existance of a package tighter.

--- script/system_setup_dsl.rb
+++ script/system_setup_dsl.rb
@@ -45,7 +45,7 @@ module DependencyDsl
 
   def ensure_package(package)
     dep package do
-      met? { `dpkg -s #{package} 2> /dev/null`.match(/\sinstalled/) }
+      met? { `dpkg -s #{package} 2> /dev/null | grep Status`.match(/\sinstalled/) }
       meet { check_status("apt-get install -qy #{package}", true, true) } # -q is quiet; -y is "answer yes"
     end
   end
