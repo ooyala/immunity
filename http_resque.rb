446e43f7c59175f7df27d54d01dbf3ab0eb180c0
Phil Crosby <philc@ooyala.com>
2012-03-12 16:43:22 -0700

    Wrap http_resque into a modular sinatra class.
    This is so it doesn't depend on Sinatra global methods like get (which conflict with fez)

--- http_resque.rb
+++ http_resque.rb
@@ -17,58 +17,64 @@
 #
 # TODO(philc): This belongs in a gem.
 
-require "sinatra"
+require "sinatra/base"
 require "thin"
 require "resque"
 require "rake"
 require "json"
 
-settings.server = "thin"
-
 # Load the Rakefile which should in turn require all of their Resque job classes.
 # TODO(philc): The path to this Rakefile should be an argument.
 load "./Rakefile"
 
-STDOUT.sync = true
-STDERR.sync = true
+class HttpResque < Sinatra::Base
+  settings.server = "thin"
 
-# Run rake resque:work in a background process. It will exit when this process exits.
-pid = fork { Rake::Task["resque:work"].invoke }
+  STDOUT.sync = true
+  STDERR.sync = true
 
-get "/" do
-  "http_resque is here."
+  # Run rake resque:work in a background process. It will exit when this process exits.
+  pid = fork { Rake::Task["resque:work"].invoke }
+
+  settings.port = ARGV.include?("-p") ? ARGV[ARGV.index("-p") + 1] : ENV["PORT"]
+
+  get "/" do
+    "http_resque is here."
+  end
+
+  # The Resque representation of up to 25 jobs in this queue, *oldest* first. Resque jobs look like this:
+  #   { "class"=>"DeployBuild", "args"=>["my_embed_code", "my_youtube_synd_id"] }
+  get "/queues/:queue/jobs" do
+    (Resque.peek(params[:queue], 0, 25) || []).to_json
+  end
+
+  delete "/queues/:queue/jobs" do
+    Resque.remove_queue(params[:queue])
+    nil
+  end
+
+  # Create a new job.
+  # - queue: the queue to enqueue this job into.
+  # - arguments: optional; an array of arguments for the Resque job.
+  post "/queues/:queue/jobs" do
+    halt(400, "Provide a valid JSON body.") unless json_body
+    klass = json_body["class"]
+    halt(400, "Specify a class.") unless klass
+    klass = Object.const_get(klass)
+    Resque.enqueue_to(params[:queue], klass, *json_body["arguments"])
+    nil
+  end
+
+  # Executes the job at the head of this queue (the oldest job), and blocks until it's finished.
+  # This is useful for scripting integration tests which verify that a background job is working correctly.
+  get "/queues/:queue/result_of_oldest_job" do
+    job = Resque::Job.reserve(params[:queue])
+    halt(404, "No jobs left in #{params[:queue]}") unless job
+    job.perform
+    nil
+  end
+
+  def json_body() @json_body ||= JSON.parse(request.body.read) rescue nil end
+
+  run! if app_file == $0
 end
-
-# The Resque representation of up to 25 jobs in this queue, *oldest* first. Resque jobs look like this:
-#   { "class"=>"DeployBuild", "args"=>["my_embed_code", "my_youtube_synd_id"] }
-get "/queues/:queue/jobs" do
-  (Resque.peek(params[:queue], 0, 25) || []).to_json
-end
-
-delete "/queues/:queue/jobs" do
-  Resque.remove_queue(params[:queue])
-  nil
-end
-
-# Create a new job.
-# - queue: the queue to enqueue this job into.
-# - arguments: optional; an array of arguments for the Resque job.
-post "/queues/:queue/jobs" do
-  halt(400, "Provide a valid JSON body.") unless json_body
-  klass = json_body["class"]
-  halt(400, "Specify a class.") unless klass
-  klass = Object.const_get(klass)
-  Resque.enqueue_to(params[:queue], klass, *json_body["arguments"])
-  nil
-end
-
-# Executes the job at the head of this queue (the oldest job), and blocks until it's finished.
-# This is useful for scripting integration tests which verify that a background job is working correctly.
-get "/queues/:queue/result_of_oldest_job" do
-  job = Resque::Job.reserve(params[:queue])
-  halt(404, "No jobs left in #{params[:queue]}") unless job
-  job.perform
-  nil
-end
-
-def json_body() @json_body ||= JSON.parse(request.body.read) rescue nil end
