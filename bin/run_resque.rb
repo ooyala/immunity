f6ee8f50f40b5d94305e85b8217f3fa4a83517ac
Phil Crosby <philc@ooyala.com>
2012-03-13 15:57:03 -0700

    Run multiple queues at once correctly. Oops.

--- bin/run_resque.rb
+++ bin/run_resque.rb
@@ -5,6 +5,6 @@ queues = %W(deploy_builds fetch_commits monitoring run_tests)
 # Note that you can use * to run jobs for all queues. We're whitelisting the queues we want workers for,
 # because we do not want to process jobs for temporary testing queues created by our integration tests.
 # TODO(philc): In the future spin up one worker per queue.
-ENV["QUEUE"] = queues.join(" ")
+ENV["QUEUE"] = queues.join(",")
 puts "Starting Resque for queues: #{queues.join(", ")}"
 exec "./http_resque.rb -p 3103", *ARGV
