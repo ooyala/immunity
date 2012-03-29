29272efe4c5387646647405d14a30138092da74a
Phil Crosby <philc@ooyala.com>
2012-03-29 16:28:17 -0700

    Remove unnecessary boilerplate

--- config/deploy_config.rb
+++ config/deploy_config.rb
@@ -1,29 +1,10 @@
-# This is the configuration file for fezzik.
-# Define variables here as you would for Vlad the Deployer.
-# A full list of variables can be found here:
-#     http://hitsquad.rubyforge.org/vlad/doco/variables_txt.html
+# This is the configuration file for Fezzik. See Fezzik's README for details (github.com/dmacdougall/fezzik).
 
 set :app, "immunity_system"
 set :deploy_to, "/opt/ooyala/#{app}"
 set :release_path, "#{deploy_to}/releases/#{Time.now.strftime("%Y%m%d%H%M")}"
 set :local_path, Dir.pwd
 set :user, "immunity"
-
-# Each destination is a set of machines and configurations to deploy to.
-# You can deploy to a destination from the command line with:
-#     fez to_dev deploy
-#
-# :domain can be an array if you are deploying to multiple hosts.
-#
-# You can set environment variables that will be loaded at runtime on the server
-# like this:
-#     Fezzik.env :rack_env, "production"
-# This will also generate a file on the server named config/environment.rb, which you can include
-# in your code to load these variables as Ruby constants. You can create your own config/environment.rb
-# file to use for development, and it will be overwritten at runtime.
-
-# This localhost target is for testing the deployment pipeline with quick turnaround times.
-#
 
 common_env_vars = {
   db_user: "root",
@@ -39,7 +20,7 @@ Fezzik.destination :vagrant do
   set :domain, "#{user}@#{hostname}"
   include_env_vars(common_env_vars)
   host "root@#{hostname}", :root_user
-  host "immunity@#{hostname}", :deploy_user
+  host "#{user}@#{hostname}", :deploy_user
 end
 
 Fezzik.destination :prod do
@@ -47,5 +28,5 @@ Fezzik.destination :prod do
   set :domain, "#{user}@#{hostname}"
   include_env_vars(common_env_vars)
   host "root@#{hostname}", :root_user
-  host "immunity@#{hostname}", :deploy_user
+  host "#{user}@#{hostname}", :deploy_user
 end
