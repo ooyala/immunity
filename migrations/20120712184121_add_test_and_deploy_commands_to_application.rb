e7878375bf0dce935c08f7f7c0eeb0b249f0e76c
Phil Crosby <philc@ooyala.com>
2012-07-12 18:53:04 -0700

    Add a migration which adds deploy_command and test_command to an application.

---
+++ migrations/20120712184121_add_test_and_deploy_commands_to_application.rb
@@ -0,0 +1,13 @@
+require "./" + File.join(File.dirname(__FILE__), "migration_helper")
+
+Sequel.migration do
+  up do
+    add_column :applications, :deploy_command, String, :size => 1024
+    add_column :applications, :test_command, String, :size => 1024
+  end
+
+  down do
+    drop_column :applications, :deploy_command
+    drop_column :applications, :test_command
+  end
+end
