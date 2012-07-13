02d9fd79253d8af0151f7e71bbcac8ebb5ed4ca3
Phil Crosby <phil.crosby@gmail.com>
2012-07-13 16:09:21 -0700

    Delete some unused columns from the builds table

---
+++ migrations/20120713160239_drop_repo_and_is_test_build_from_builds.rb
@@ -0,0 +1,13 @@
+require "./" + File.join(File.dirname(__FILE__), "migration_helper")
+
+Sequel.migration do
+  up do
+    drop_column :builds, :is_test_build
+    drop_column :builds, :repo
+  end
+
+  down do
+    add_column :builds, :is_test_build, TrueClass, :default => false
+    add_column :builds, :repo, String
+  end
+end
